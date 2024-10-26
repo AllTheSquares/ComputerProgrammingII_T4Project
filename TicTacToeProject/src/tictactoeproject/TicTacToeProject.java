b251972fe696ce79707fa2c345a2fbc5970333af
Ryan Ruiz <ruiz.ryan.nj@gmail.com>
2024-10-26 09:58:54 -0400

    Initialized board

--- TicTacToeProject/src/tictactoeproject/TicTacToeProject.java
+++ TicTacToeProject/src/tictactoeproject/TicTacToeProject.java
@@ -1,20 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+Wail Chaqour, Isai Rojas, Ryan Ruiz
+Tic Tac Toe Project 2024
+December 7, 2024
+*/
+
 package tictactoeproject;
 
-/**
- *
- * @author allthesquares
- */
-public class TicTacToeProject {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+class TicTacToeProject {
     public static void main(String[] args) {
-        // TODO code application logic here
+        public static char[][] board = new char[3][3];
     }
+    
     
 }
