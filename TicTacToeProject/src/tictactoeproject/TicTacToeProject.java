52fefb4462c9682fc6c45ac0a9e537f95b49457a
Ryan Ruiz <ruiz.ryan.nj@gmail.com>
2024-10-26 10:00:16 -0400

    dudes i'm an idiot for doing public in a public function LMFAOOOOO

--- TicTacToeProject/src/tictactoeproject/TicTacToeProject.java
+++ TicTacToeProject/src/tictactoeproject/TicTacToeProject.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 class TicTacToeProject {
     public static void main(String[] args) {
-        public static char[][] board = new char[3][3];
+        char[][] board = new char[3][3];
     }
     
     
